8be9cc7953778eaa3c7cc95616427aca764aada8
Tejas Nanaware <nanawaret@hotmail.com>
2018-07-03 07:33:44 +0530

    Split word and meaning

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { A } from './assets/wordlist.json';
 
 class App extends Component {
+	constructor() {
+		super();
+		this.word = A[Math.floor(Math.random()*A.length)].split(":");
+	}
 	render() {
 		return (
 			<div className='container'>
 				<h1>Hello</h1>
-				<p style={{'fontFamily': 'Raleway'}} >{ A[Math.floor(Math.random()*A.length)] }</p>
+				<p style={{'fontFamily': 'Raleway'}} >{ this.word[0] }</p>
+				<p style={{'fontFamily': 'Raleway'}} >{ this.word[1] }</p>
 			</div>
 		);
 	}
