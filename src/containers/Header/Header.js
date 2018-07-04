0ae030453f2d550aef9276860778bcde01a846c6
Tejas Nanaware <nanawaret@hotmail.com>
2018-07-05 00:45:22 +0530

    Using HashRouter instead of BrowserRouter

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -4,23 +4,23 @@ import { Link } from 'react-router-dom';
 const Header = () => {
 	return (
 		<nav className="navbar navbar-expand-md bg-dark navbar-dark">
-			<Link to='/' className='navbar-brand'>Vocabulary App</Link>
+			<Link to='/' replace className='navbar-brand'>Vocabulary App</Link>
 			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 				<span className="navbar-toggler-icon"></span>
 			</button>
 			<div className="collapse navbar-collapse" id="collapsibleNavbar">
 				<ul className="navbar-nav">
 					<li className="nav-item">
-						<Link to='/' className="nav-link">Home</Link>
+						<Link to='/' replace className="nav-link">Home</Link>
 					</li>
 					<li className="nav-item">
-						<Link to='/single' className="nav-link">Single Word</Link>
+						<Link to='/single' replace className="nav-link">Single Word</Link>
 					</li>
 					<li className="nav-item">
-						<Link to='/list' className="nav-link">List of Words</Link>
+						<Link to='/list' replace className="nav-link">List of Words</Link>
 					</li>
 					<li className="nav-item">
-						<Link to='/about' className="nav-link">About</Link>
+						<Link to='/about' replace className="nav-link">About</Link>
 					</li>
 				</ul>
 			</div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
 registerServiceWorker();
