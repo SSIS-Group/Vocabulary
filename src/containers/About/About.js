fe3024690b7f5db5502c88929184ddbca5574360
Tejas-Nanaware <nanawaret@hotmail.com>
2018-10-24 17:34:20 +0530

    Updates

--- src/containers/About/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const About = () => {
-	return(
-		<div>App made by Tejas Nanaware. Visit GitHub Profile.</div>
-	);
-};
-
-export default About;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { HashRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-registerServiceWorker();
